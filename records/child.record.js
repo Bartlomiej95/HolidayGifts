2ccaf29f22f3014466db6ff1e428baf0e23bfa62
Bartlomiej95 <panfilbartlomiej@gmail.com>
2022-01-06 19:34:43 +0100

    Add func: insert,getAll/One,update to records
    Add the first function from CRUD to records:
    getAll - show all records from database
    getOne - show one record from database by id
    insert - add new record to database
    update - update record in database by id

--- records/child.record.js
+++ records/child.record.js
@@ -1,17 +1,52 @@
+const {ValidationError} = require("../utils/errors");
+const {pool} = require("../utils/db");
+const {v4:uuid} = require('uuid');
+
 class ChildRecord {
-    static listAll() {
-        return [
-            {
-                id: 'abc1',
-                name: 'Ania',
-                gift: 'Domek dla lalek'
-            },
-            {
-                id: 'efg2',
-                name: 'Piotrek',
-                gift: 'Samochodzik brum brum'
-            }
-        ]
+
+    constructor(obj) {
+
+        if(!obj.name || obj.name < 2 || obj.name > 26) {
+            throw new ValidationError('Name should have more than 2 characters and less than 26')
+        }
+
+        this.id = obj.id;
+        this.name = obj.name;
+        this.giftId = obj.giftId;
+    }
+
+    static async listAll() {
+
+        const [results] = await pool.execute("SELECT * FROM `children` ORDER BY `name` ASC");
+        return results.map(obj => new ChildRecord(obj));
+
+    }
+
+    async insert() {
+        if(!this.id) {
+            this.id = uuid();
+        }
+
+        await pool.execute("INSERT INTO `children`(`id`, `name`) VALUES(:id, :name) ", {
+            id: this.id,
+            name: this.name,
+        })
+
+    }
+
+    static async getOne(id) {
+        const [results] = await pool.execute("SELECT * FROM `children` WHERE `id` = :id ", {
+            id,
+        });
+        return results.length === 0 ? null : new ChildRecord(results[0]);
+    }
+
+    async update(){
+        await pool.execute("UPDATE `children` SET `name` = :name, `giftId` = :giftId WHERE `id` = :id ", {
+            id: this.id,
+            name: this.name,
+            giftId: this.giftId,
+        })
     }
 }
 
--- records/gift.record.js
+++ records/gift.record.js
@@ -1,17 +1,55 @@
+const {ValidationError} = require("../utils/errors");
+const {pool} = require("../utils/db");
+const {v4:uuid} = require('uuid');
+
 class GiftRecord {
-    static listAll() {
-        return [
-            {
-                id: 'ab12',
-                name: 'Domek dla lalek',
-                count: 3,
-            },
-            {
-                id: 'gr43',
-                name: 'Samochodzik brum brum',
-                count: 4
-            }
-        ]
+
+    constructor(obj) {
+        if(!obj.name || obj.name.length < 3 || obj.name.length > 55 ) {
+            throw new ValidationError('Gift should have more than 3 characters and less than 55 characters');
+        }
+
+        if(!obj.count || obj.count < 1 || obj.count > 999999) {
+            throw new ValidationError('Amount should be in range 1 - 999999')
+        }
+
+        this.id = obj.id;
+        this.name = obj.name;
+        this.count = obj.count;
+    }
+
+    static async listAll() {
+
+        const [results] = await pool.execute("SELECT * FROM `gifts` ");
+        return results.map(obj => new GiftRecord(obj));
+    }
+
+    async insert(){
+        if(!this.id){
+            this.id = uuid();
+        }
+
+        await pool.execute("INSERT INTO `gifts`(`id`, `name`, `count`) VALUES(:id, :name, :count)", {
+            id: this.id,
+            name: this.name,
+            count: this.count,
+        });
+
+        return this.id;
+    }
+    static async getOne(id) {
+        const [results] = await pool.execute("SELECT * FROM `gifts` WHERE `id` = :id ", {
+            id,
+        });
+        return results.length === 0 ? null : new GiftRecord(results[0]);
+    }
+
+    async countGivenGifts() {
+        const [[{count}]] = await pool.execute("SELECT COUNT(*) AS `count` FROM `children` WHERE `giftId` = :id ", {
+            id: this.id
+        });
+
+        return count;
     }
 }
 
